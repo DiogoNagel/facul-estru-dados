13735e6639df854bde7cd47eba22468e8e4183d8
Herykson Gomes <heryksonsouza@gmail.com>
2016-03-17 15:03:41 -0300

    Delete Atividade.java

--- Atividade.java
+++
@@ -1,4 +0,0 @@
-//Métodos:
-//Crie uma classe chamada Gerente para definir os objetos que representarão os gerentes do
-//banco. Defina dois métodos de aumento salarial nessa classe. O primeiro deve aumentar o salário
-//com uma taxa fixa de 10%. O segundo deve aumentar o salário com uma taxa variável. Depois, teste os métodos de aumento salarial definidos na classe Gerente.
